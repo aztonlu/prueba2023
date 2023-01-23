b755b675deae9b6f245fcbeb64ec9070bb0c4df5
Luis Correa <jluiscorrea90@outlook.com>
2023-01-23 13:04:57 -0500

    prueba solo creacion primera tabla tareas

---
+++ database/migrations/2023_01_23_135532_create_tareas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',60);
+            $table->string('descripcion')->nullable();
+            $table->tinyInteger('finalizada')->default(0);
+            $table->timestamp('fechalimite');
+            $table->tinyInteger('urgencia')->coment('0: No es urgente, 1: Urgencia normal, 2: Muy urgente');
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
